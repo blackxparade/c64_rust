34510f06f033f2a9e612eb8d7a9a9c5b3eb4747f
Krizsák Tibor <tibor.krizsak@gmail.com>
2019-01-27 11:46:29 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+extern crate rand;
+extern crate sfml;
+
+use sfml::graphics::*;
+use sfml::window::*;
+use rand::Rng;
+
+
+fn main() {
+    let mut window = RenderWindow::new(
+        (800, 600),
+        "this is a title.",
+        Style::TITLEBAR | Style::CLOSE,
+        &Default::default(),
+    );
+    window.set_vertical_sync_enabled(true);
+
+    let mut rng = rand::thread_rng();
+    let mut pixels: [u8; 80 * 60 * 4] = [150; 80 * 60 * 4];
+
+    let font = Font::from_file("res/C64_pro.ttf").unwrap();
+    let msg = "YAY this shit is working";
+    let mut instructions = Text::new(msg, &font, 30);
+    instructions.set_position((100.0, 280.0));
+    instructions.set_fill_color(&Color::WHITE);
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            use crate::Event::*;
+            match event {
+                Closed => window.close(),
+                KeyPressed { code, .. } => match code {
+                    Key::Escape => window.close(),
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+
+        for i in 0..(80 * 60 * 4) {
+            pixels[i] = rng.gen_range(0, 255);
+        }
+
+        let mut texture = Texture::new(800, 600).unwrap();
+        texture.update_from_pixels(&pixels, 80, 60, 0, 0);
+        let mut sprite = Sprite::with_texture(&texture);
+        sprite.set_position((0., 0.));
+        sprite.set_scale((10.0, 10.0));
+
+        window.clear(&Color::BLACK);
+        window.draw(&sprite);
+        window.draw(&instructions);
+        window.display();
+    }
+}
