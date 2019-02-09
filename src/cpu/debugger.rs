14d771a3bf3f515efbe40f862e1cd1e40aaa74bd
Krizsák Tibor <tibor.krizsak@gmail.com>
2019-02-09 17:11:14 +0100

    Debugger polishing.

--- src/cpu/debugger.rs
+++ src/cpu/debugger.rs
@@ -73,65 +73,68 @@ impl Debugger {
 
     pub fn assemble_text(&mut self, text: Vec<String>) {
         self.line_count = text.len() as u8;
-        const BG_WIDTH  : u32 = 1000;
+        const BG_WIDTH  : u32 = 1015;
         const BG_HEIGHT : u32 = 30;
-        let mut text_background: [u8; BG_WIDTH as usize * BG_HEIGHT as usize * 4] =
-                                 [255; BG_WIDTH as usize * BG_HEIGHT as usize * 4];
 
-        for i in (0..text_background.len()).step_by(4) {
-            text_background[i]   = 134;
-            text_background[i+1] = 122;
-            text_background[i+2] = 221;
-            text_background[i+3] = 255;
-        }
+        // main texture pixel array and object
+        let mut texture_pixels: [u8; 4] = [134, 122, 221, 255];
+        let mut texture = Texture::new(1, 1).unwrap();
+        texture.update_from_pixels(&texture_pixels, 1, 1, 0, 0);
 
-        let mut text_background_texture = Texture::new(BG_WIDTH, BG_HEIGHT).unwrap();
-        text_background_texture.update_from_pixels(&text_background, BG_WIDTH, BG_HEIGHT, 0, 0);
-        let mut text_background_sprite = Sprite::with_texture(&text_background_texture);
-        let mut header_background_sprite = Sprite::with_texture(&text_background_texture);
+        // separator sprite based on the generic texture with scaling
+        let mut separator_sprite = Sprite::with_texture(&texture);
+        separator_sprite.set_position((1011.0, 0.0));
+        separator_sprite.set_scale((4.0, 1600.0));
 
-        let mut render_text_header = Text::new("    PC    |             CPU INSTRUCTION            ",
-                                               &self.font, 22);
+        // text background sprite based on the generic texture with scaling
+        let mut text_background_sprite = Sprite::with_texture(&texture);
+        text_background_sprite.set_scale((BG_WIDTH as f32, BG_HEIGHT as f32));
 
-        header_background_sprite.set_position((11.0, 2.0));
+        // header background sprite based on the generic texture with scaling
+        // and rendering
+        let mut header_background_sprite = Sprite::with_texture(&texture);
+        header_background_sprite.set_position((0.0, 0.0));
+        header_background_sprite.set_scale((BG_WIDTH as f32, BG_HEIGHT as f32));
         self.dbg.draw(&header_background_sprite);
 
-        render_text_header.set_position((15.0, 5.0));
+        // header text initialization and rendering
+        let mut render_text_header = Text::new("    PC    |           CPU INSTRUCTION              ",
+                                               &self.font, 22);
+        render_text_header.set_position((15.0, 3.0));
         render_text_header.set_fill_color(&self.DARK_BLUE);
         self.dbg.draw(&render_text_header);
 
-
+        // text lines positioning, rendering, and text background also
         for i in 0..text.len() {
             let mut render_text = Text::new(&text[i], &self.font, 22);
             render_text.set_position((15.0, i as f32 * 32.0 + 37.0));
 
             if (i == self.active_state as usize) {
                 render_text.set_fill_color(&self.DARK_BLUE);
-                text_background_sprite.set_position((11.0, i as f32 * 32.0 + 34.0));
+                text_background_sprite.set_position((0.0, i as f32 * 32.0 + 34.0));
                 self.dbg.draw(&text_background_sprite);
+
             } else {
                 render_text.set_fill_color(&self.LIGHT_BLUE);
             }
-
             self.dbg.draw(&render_text);
         }
+        self.dbg.draw(&separator_sprite);
     }
 
     pub fn memory_map(&mut self, ram: &[u8]) {
 
         let mut pixels: [u8; 256 * 256 * 4] = [255; 256 * 256 * 4];
+        let light_blue = [134, 122, 221, 255];
+        let dark_blue = [72, 59, 170, 255];
 
+        // it seemed like it's faster when not using close from slice, only
+        // regular value copy to a specific array index
         for i in 0..ram.len() {
             if (ram[i] != 0) {
-                pixels[i*4] = 134;
-                pixels[i*4+1] = 122;
-                pixels[i*4+2] = 221;
-                pixels[i*4+3] = 255;
+                pixels[(i*4)..(i*4+4)].clone_from_slice(&light_blue);
             } else {
-                pixels[i*4] = 72;
-                pixels[i*4+1] = 59;
-                pixels[i*4+2] = 170;
-                pixels[i*4+3] = 255;
+                pixels[(i*4)..(i*4+4)].clone_from_slice(&dark_blue);
             }
         }
 
